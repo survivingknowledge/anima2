42d007a73692ecc92bf000b64494cc8adf2a767d
Jordan Sleeper <survivingknowledge@gmail.com>
2017-07-11 22:49:12 -0400

    Created basic chapter controller

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,2 +1,47 @@
 class Chapter < ApplicationRecord
+
+  before_action :set_chapter, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @chapters = Chapter.all
+  end
+
+  def new
+    @chapter = Chapter.new
+  end
+
+  def create
+    @chapter = Chapter.new(chapter_params)
+    if @chapter.save
+      redirect_to chapter_path(@chapter)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @chapter.update(chapter_params)
+      redirect_to chapter_path(@chapter)
+    else
+      render :edit
+    end
+  end
+
+
+
+  private
+
+  def set_chapter
+    @chapter = Chapter.find(params[:id])
+  end
+
+  def chapter_params
+    params.require(:chapter).permit(:title, :chapter_number, :user_id, :status)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'novels#index'
 
   resources :novels, param: :slug do
-    resources :chapters
-    get '/:chapter_num', to: 'chapter#show'
+    get '/:chapter_number', to: 'chapters#show'
+    # resources :chapters
   end
+
 end
